24cf292a4aa6e4db388542a9ee3a99c6481a9a35
zereentabassum <islamms262@gmail.com>
2022-05-24 00:27:54 +0600

    middle ware and cors dotenv config imported

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json()); 
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
