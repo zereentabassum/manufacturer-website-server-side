28babace7e477d48a0ff18e7801c69925afb3f98
zereentabassum <islamms262@gmail.com>
2022-05-24 01:31:48 +0600

    database connection setup done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 const port = process.env.PORT || 5000
 
@@ -8,6 +10,24 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json()); 
+
+
+const uri = "mongodb+srv://manufacturer:<password>@cluster0.pjhca.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection = client.db('inventory').collection('item');
+
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
